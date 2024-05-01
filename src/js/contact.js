087171852ddbe316ff66aacad98d4d1855385123
Origami <spb.origami@gmail.com>
2024-05-01 13:45:00 +0300

    Created contact: hero section and added contact:partners

---
+++ src/js/contact.js
@@ -0,0 +1,17 @@
+import '../assets/scss/contact.scss';
+
+// Components
+import { useTheme } from './components/theme.js';
+useTheme();
+
+import { useBurger } from './components/burger.js';
+useBurger();
+
+import { useHeader } from './components/header.js';
+useHeader();
+
+import { useMap } from './components/contact/map.js';
+useMap();
+
+import { usePartnersSlider } from './components/home/slider.js';
+usePartnersSlider();
